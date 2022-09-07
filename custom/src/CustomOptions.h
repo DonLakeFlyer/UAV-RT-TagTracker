555d186552e2a900f6f41f8bc1887c6f528c5cd7
Don Gagne <don@thegagnes.com>
2022-09-07 16:08:27 -0700

    Remove unused variable

--- custom/src/CustomOptions.h
+++ custom/src/CustomOptions.h
@@ -10,7 +10,4 @@ public:
     CustomOptions(CustomPlugin* plugin, QObject* parent = NULL);
 
     //QUrl flyViewOverlay  (void) const { return QUrl::fromUserInput("qrc:/qml/VHFTrackerFlyViewOverlay.qml"); }
-
-private:
-    CustomPlugin*  _vhfQGCPlugin;
 };
