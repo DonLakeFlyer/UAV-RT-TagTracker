e3d5ded0d0f1981705512eff90bf269c07cb982e
Don Gagne <don@thegagnes.com>
2022-11-05 15:07:15 -0700

    Confirmed/Unconfirmed show different pulse icon Fixes

--- custom/src/CustomFirmwarePlugin.cc
+++ custom/src/CustomFirmwarePlugin.cc
@@ -4,14 +4,3 @@ CustomFirmwarePlugin::CustomFirmwarePlugin(void)
 {
 
 }
-
-const QVariantList& CustomFirmwarePlugin::toolIndicators(const Vehicle* vehicle)
-{
-    //-- Default list of indicators for all vehicles.
-    if(_toolIndicatorList.size() == 0) {
-        _toolIndicatorList = FirmwarePlugin::toolIndicators(vehicle);
-
-        _toolIndicatorList += QVariant::fromValue(QUrl::fromUserInput("qrc:/toolbar/PulseIndicator.qml"));
-    }
-    return _toolIndicatorList;
-}
