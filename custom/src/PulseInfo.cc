8c8526c577615123caab524170716b990ddead0f
Don Gagne <don@thegagnes.com>
2023-03-01 10:25:27 -0800

    Tag name, rate name support

--- custom/src/PulseInfo.cc
+++ custom/src/PulseInfo.cc
@@ -1,8 +1,10 @@
 #include "PulseInfo.h"
 
-PulseInfo::PulseInfo(TunnelProtocol::PulseInfo_t pulseInfo, QObject* parent)
+PulseInfo::PulseInfo(TunnelProtocol::PulseInfo_t pulseInfo, QString& name, QString& rateChar, QObject* parent)
     : QObject   (parent)
     , _pulseInfo(pulseInfo)
+    , _name     (name)
+    , _rateChar (rateChar)
 {
 
 }
--- custom/src/PulseInfo.h
+++ custom/src/PulseInfo.h
@@ -10,21 +10,27 @@ class PulseInfo : public QObject
     Q_OBJECT
 
 public:
-    PulseInfo(TunnelProtocol::PulseInfo_t pulseInfo, QObject* parent = NULL);
+    PulseInfo(TunnelProtocol::PulseInfo_t pulseInfo, QString& name, QString& rateChar, QObject* parent = NULL);
     ~PulseInfo();
 
     Q_PROPERTY(uint     tag_id              READ tag_id             CONSTANT)
+    Q_PROPERTY(QString  name                READ name               CONSTANT)
+    Q_PROPERTY(QString  rateChar            READ rateChar               CONSTANT)
     Q_PROPERTY(uint     frequency_hz        READ frequency_hz       CONSTANT)
     Q_PROPERTY(double   start_time_seconds  READ start_time_seconds CONSTANT)
     Q_PROPERTY(double   snr                 READ snr                CONSTANT)
     Q_PROPERTY(uint     group_ind           READ group_ind          CONSTANT)
 
-    uint    tag_id             (void) { return _pulseInfo.tag_id; }
-    uint    frequency_hz       (void) { return _pulseInfo.frequency_hz; }
-    double  start_time_seconds (void) { return _pulseInfo.start_time_seconds; }
-    double  snr                (void) { return _pulseInfo.snr; }
-    uint    group_ind          (void) { return _pulseInfo.group_ind; }
+    uint    tag_id              (void) { return _pulseInfo.tag_id; }
+    QString name                (void) { return _name; }
+    QString rateChar            (void) { return _rateChar; }
+    uint    frequency_hz        (void) { return _pulseInfo.frequency_hz; }
+    double  start_time_seconds  (void) { return _pulseInfo.start_time_seconds; }
+    double  snr                 (void) { return _pulseInfo.snr; }
+    uint    group_ind           (void) { return _pulseInfo.group_ind; }
 
 private:
     TunnelProtocol::PulseInfo_t _pulseInfo;
+    QString                     _name;
+    QString                     _rateChar;
 };
