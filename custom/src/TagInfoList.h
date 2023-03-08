54ada1752e62adb7dd18bc990cbc267460079720
Don Gagne <don@thegagnes.com>
2023-03-08 15:51:26 -0800

    Tuner support

--- custom/src/TagInfoList.h
+++ custom/src/TagInfoList.h
@@ -16,21 +16,24 @@ typedef struct {
 class TagInfoList : public QList<ExtendedTagInfo_t>
 {
 public:
+    TagInfoList();
+
     bool                loadTags        (void);
     ExtendedTagInfo_t   getTagInfo      (uint32_t tag_id);
     uint32_t            radioCenterHz   () { return _radioCenterHz; }
 
 private:
     bool _channelizerTuner  ();
-    void _printChannelMap   (const int centerFreq, const QVector<int>& requestedFreqsHz);
+    void _printChannelMap   (const uint32_t centerFreqHz, const QVector<uint32_t>& wrappedRequestedFreqsHz);
     int  _firstChannelFreqHz(const int centerFreq);
 
-    uint32_t    _radioCenterHz      = 0;
+    uint32_t            _radioCenterHz      = 0;
+    QVector<uint32_t>   _channelBucketCenters;
 
-    static const int   _sampleRateHz       = 100000;
-    static const int   _nChannels          = 10;
-    static const int   _fullBwHz           = _sampleRateHz;
-    static const int   _halfBwHz           = _fullBwHz / 2;
-    static const int   _channelBwHz        = _sampleRateHz / _nChannels;
-    static const int   _halfChannelBwHz    = _channelBwHz / 2;
+    static const uint32_t   _sampleRateHz       = 375000;
+    static const uint       _nChannels          = 100;
+    static const uint32_t   _fullBwHz           = _sampleRateHz;
+    static const uint32_t   _halfBwHz           = _fullBwHz / 2;
+    static const uint32_t   _channelBwHz        = _sampleRateHz / _nChannels;
+    static const uint32_t   _halfChannelBwHz    = _channelBwHz / 2;
 };
