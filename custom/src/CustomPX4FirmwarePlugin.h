1522514cb626a4a8075ed8e9a0617333b1fca6f5
Don Gagne <don@thegagnes.com>
2023-07-18 13:30:18 -0700

    ArduPilot support
    Also fixed crash on Stop Detection

---
+++ custom/src/CustomPX4FirmwarePlugin.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "FirmwarePlugin.h"
+#include "PX4FirmwarePlugin.h"
+
+class CustomPX4FirmwarePlugin : public PX4FirmwarePlugin
+{
+    Q_OBJECT
+
+public:
+    CustomPX4FirmwarePlugin(void);
+
+    // FirmwarePlugin overrides
+    const QVariantList& toolIndicators(const Vehicle* vehicle) final;
+
+private:
+    QVariantList _toolIndicatorList;
+};
