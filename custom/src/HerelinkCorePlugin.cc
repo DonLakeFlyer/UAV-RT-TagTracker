5b650d7623802296ae6fe0f5f757909d6af24e30
Don Gagne <don@thegagnes.com>
2023-07-19 13:09:35 -0700

    AutoConnect settings overrides for AirUnit

--- custom/src/HerelinkCorePlugin.cc
+++ custom/src/HerelinkCorePlugin.cc
@@ -1,5 +1,9 @@
 #include "HerelinkCorePlugin.h"
 
+#include "AutoConnectSettings.h"
+
+#include <list>
+
 QGC_LOGGING_CATEGORY(HerelinkCorePluginLog, "HerelinkCorePluginLog")
 
 HerelinkCorePlugin::HerelinkCorePlugin(QGCApplication *app, QGCToolbox* toolbox)
@@ -13,4 +17,42 @@ void HerelinkCorePlugin::setToolbox(QGCToolbox* toolbox)
     QGCCorePlugin::setToolbox(toolbox);
 
     _herelinkOptions = new HerelinkOptions(this, nullptr);
+}
+
+bool HerelinkCorePlugin::overrideSettingsGroupVisibility(QString name)
+{
+    // Hide all AutoConnect settings
+    return name != AutoConnectSettings::name;
+}
+
+bool HerelinkCorePlugin::adjustSettingMetaData(const QString& settingsGroup, FactMetaData& metaData)
+{
+    if (settingsGroup == AutoConnectSettings::settingsGroup) {
+        // We have to adjust the Herelink UDP autoconnect settings for the AirLink
+        if (metaData.name() == AutoConnectSettings::udpListenPortName) {
+            metaData.setRawDefaultValue(14551);
+        } else if (metaData.name() == AutoConnectSettings::udpTargetHostIPName) {
+            metaData.setRawDefaultValue(QStringLiteral("127.0.0.1"));
+        } else if (metaData.name() == AutoConnectSettings::udpTargetHostPortName) {
+            metaData.setRawDefaultValue(15552);
+        } else {
+            // Disable all the other autoconnect types
+            const std::list<const char *> disabledAndHiddenSettings = {
+                AutoConnectSettings::autoConnectPixhawkName,
+                AutoConnectSettings::autoConnectSiKRadioName,
+                AutoConnectSettings::autoConnectPX4FlowName,
+                AutoConnectSettings::autoConnectRTKGPSName,
+                AutoConnectSettings::autoConnectLibrePilotName,
+                AutoConnectSettings::autoConnectNmeaPortName,
+                AutoConnectSettings::autoConnectZeroConfName,
+            };
+            for (const char * disabledAndHiddenSetting : disabledAndHiddenSettings) {
+                if (disabledAndHiddenSetting == metaData.name()) {
+                    metaData.setRawDefaultValue(false);
+                }
+            }
+        }
+    }
+
+    return true; // Show all settings in ui
 }
