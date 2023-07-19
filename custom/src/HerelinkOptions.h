a04360a3785d4437afce586649e17fb443e5b83b
Don Gagne <don@thegagnes.com>
2023-07-19 13:09:53 -0700

    Options overrides

--- custom/src/HerelinkOptions.h
+++ custom/src/HerelinkOptions.h
@@ -8,4 +8,9 @@ class HerelinkOptions : public QGCOptions
 {
 public:
     HerelinkOptions(HerelinkCorePlugin* plugin, QObject* parent = NULL);
+
+    // QGCOptions overrides
+    bool wifiReliableForCalibration () const final { return true; }
+    bool showFirmwareUpgrade        () const final { return false; }
+    bool multiVehicleEnabled        () const final { return false; }
 };
